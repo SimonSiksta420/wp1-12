57d9cc91b0698e850b204cf54b8d41841b0d9388
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-05-12 10:04:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,13 +9,13 @@
 <form action="submit.php" method="post">
 <label for="i-firstname"> Firstname: </label>
 <input type="text" name="firstname" id="">
-
+<br>
 <label for="i-lastname"> Surname: </label>
 <input type="text" name="lastname" id="">
-
+<br>
 <label for="i-birthday"> Your birthday: </label>
 <input type="number" min="1900" max="2020" name="birthday" id="">
-
+<br>
 
 
 <input type="submit" value="odeslat">
